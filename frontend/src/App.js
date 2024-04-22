dfe6d1be664356ba18aca2ba2274ae4579b5f8cc
twezowic <01169258@pw.edu.pl>
2024-04-22 21:26:22 +0200

    Change the look of app to match the one from figma

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import React, { useEffect, useState } from "react";
 import DataComponent from './components/Hello';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <header className="App-header">
         <DataComponent />
       </header>
