293aaa429cea61402b1676484b534f692a1d0c39
twezowic <01169258@pw.edu.pl>
2024-05-06 23:18:42 +0200

    Begin handling the link of presentation

--- frontend/src/components/LectureView.js
+++ frontend/src/components/LectureView.js
@@ -1,6 +1,11 @@
 import React, { useState } from 'react';
 import "./LectureView.css"
 
+function getSrc(html) {
+    var regex = /src="([^"]+)"/;
+    return html.match(regex)[1];
+}
+
 const LectureView = () => {
     const [title, setTitle] = useState("temp");
     const [date, setDate] = useState("01.01.2000");
@@ -12,15 +17,17 @@ const LectureView = () => {
         console.log(question);
     }
 
+    const htmllink = '<iframe src="https://1drv.ms/p/c/77287afd4195c30f/IQMPw5VB_XooIIB3dAYAAAAAATZKsZ-Zjucl6tGxFUc8pfM" width="402" height="327" frameborder="0" scrolling="no"></iframe>'
+    const link = getSrc(htmllink) + "?em=2&amp;wdAr=1.7777777777777777&amp;wdEaaCheck=1"
+
     return (
         <div className="component-container-lectureview">
             <div className="component-title-lectureview">{title}{date}</div>
             <div className='divider-lectureview'>
                 <div className="element-lectureview">
                     <div className="transcription">{transcription}</div>
-                    <iframe className='presentation'
-                        src="https://1drv.ms/p/c/77287afd4195c30f/IQMPw5VB_XooIIB3dAYAAAAAATZKsZ-Zjucl6tGxFUc8pfM?em=2&amp;wdAr=1.7777777777777777&amp;wdEaaCheck=1"
-                        width="730px" height="270px">To jest osadzony dokument pakietu <a target="_blank" href="https://office.com">Microsoft Office</a> obsługiwany przez aplikację <a target="_blank" href="https://office.com/webapps">Office</a>.
+                    <iframe className="presentation"
+                        src="https://1drv.ms/p/c/77287afd4195c30f/IQMPw5VB_XooIIB3dAYAAAAAATZKsZ-Zjucl6tGxFUc8pfM?em=2&amp;wdAr=1.7777777777777777&amp;wdEaaCheck=1" width="730px" height="270px">
                     </iframe>
 
                 </div>
@@ -51,4 +58,15 @@ export default LectureView;
 
 
 // Prezentacje trzeba ogarnąć żeby poprawnie link przyjmować a nie całego html
+
+// nie wiem czy to bezpieczne
+// Instrukcja:
+// Osadź
+// skopiuj komponent iframe
+// wklej
+// np. <iframe src="https://1drv.ms/p/c/77287afd4195c30f/IQMPw5VB_XooIIB3dAYAAAAAATZKsZ-Zjucl6tGxFUc8pfM" width="402" height="327" frameborder="0" scrolling="no"></iframe>
+// bierzemy stąd tylko src
+
+// Trzeba przekzywać link do iframe tylko póki co jak jest przekazywane nie przez wartość to rozdzielczość spada
+
 // Dodać możliwość nie wyświetlania/chowania prezentacji
