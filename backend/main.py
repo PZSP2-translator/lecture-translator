aad070b3a10647df22bc5782aff61be65d339acd
pgrabow1 <01149927@pw.edu.pl>
2024-05-29 19:51:08 +0200

    added code and api and deleted obsolete code

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,9 @@
 import sounddevice as sd
 from scipy.io.wavfile import write
 import tempfile
+import argparse
 
-from pydub import AudioSegment # pip install pydub sudo apt install ffmpeg
+from pydub import AudioSegment
 from pydub import silence as S
 
 import transcribe
@@ -18,49 +19,54 @@ def choose_cutting_point(indata):
         filename = tmpfile.name
         write(filename, G.FREQ, indata)
         song = AudioSegment.from_wav(filename)
-        dBFS=song.dBFS
+        dBFS = song.dBFS
         silence = S.detect_silence(song, min_silence_len=100, silence_thresh=dBFS-60)
         if len(silence) <= 1:
             cutting_frame = len(indata)
         else:
-            cutting_frame = ((silence[-1][1] - silence[-1][0])//2 + silence[-1][0]) * G.FREQ // 1000 # converting from ms to s
+            cutting_frame = ((silence[-1][1] - silence[-1][0]) // 2 + silence[-1][0]) * G.FREQ // 1000  # converting from ms to s
     return cutting_frame
 
-def transcribe_fragment(data): # currently making a temporary file, test giving data after dividing by 32768.0 as stated below
-# Convert in-ram buffer to something the model can use directly without needing a temp file.
-# Convert data from 16 bit wide integers to floating point with a width of 32 bits.
-# Clamp the audio stream frequency to a PCM wavelength compatible default of 32768hz max.
-#audio_np = np.frombuffer(audio_data, dtype=np.int16).astype(np.float32) / 32768.0
-
+def transcribe_fragment(data, model):
     result = None
     with tempfile.NamedTemporaryFile(suffix='.wav', delete=True) as tmpfile:
         filename = tmpfile.name
-        array_to_translate = np.concatenate((last_data_buffer, data), axis=0)
-        write(filename, G.FREQ, array_to_translate)
-        result = model.transcribe(filename, language = "pl", fp16=False)
+        write(filename, G.FREQ, data)
+        result = model.transcribe(filename, language="pl", fp16=False)
     return result["text"]
 
-def callback(indata, frames, time, status):
+def callback(indata, frames, time, status, model, ip_address, lecture_code):
     global last_data_buffer
     cutting_frame = choose_cutting_point(indata)
-    text = transcribe_fragment(indata[:cutting_frame])
+    text = transcribe_fragment(indata[:cutting_frame], model)
     if text == None or text == "":
         return
     last_data_buffer = indata[cutting_frame:]
     text = translate.translate_pl_to_en(text)
-    #print(text)
-    sender.send_text(text)
+    sender.send_text(text, ip_address, lecture_code)
 
-if __name__ == "__main__":
-    model = transcribe.get_model()
+
+def main(lecture_code, ip_address):
+    print(f"Starting transcription for lecture: {lecture_code} at IP: {ip_address}")
+    print(sd.query_devices())
+
+    model = transcribe.get_model() 
+    global last_data_buffer
     last_data_buffer = np.zeros([0, 1], dtype='float32')
 
-    with sd.InputStream(device=1,
-                samplerate=G.FREQ, blocksize=G.FREQ * G.BLOCK_LENGTH_SEC,
-                channels=1, callback=callback):
+    with sd.InputStream(device=1, samplerate=G.FREQ, blocksize=G.FREQ * G.BLOCK_LENGTH_SEC,
+                        channels=1, callback=lambda indata, frame, time, status: callback(indata, frame, time, status, model)):
         time.sleep(5)
         print("Microphone started")
         print("#" * 80)
-        print("press Enter to quit")
+        print("Press Enter to quit")
         print("#" * 80)
         input()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Script to transcribe lectures.")
+    parser.add_argument("lecture_code", type=str, help="Code of the lecture to transcribe.", default="TESTcode", nargs='?')
+    parser.add_argument("ip_address", type=str, help="IP address for sending transcriptions.", default="api", nargs='?')
+
+    args = parser.parse_args()
+    main(args.lecture_code, args.ip_address)
--- backend/sender.py
+++ backend/sender.py
@@ -1,37 +1,6 @@
-# import multiprocessing
-import time
 import requests
 
-import random
-def sender():
-    count = 0
-    while True:
-        count += 1
-        data = {"text": random_sentence_in_polish()}
-        response = requests.post("http://127.0.0.1:5000/", json=data)
-        print(f"Sent: {count}, Response: {response.json()}")
-        time.sleep(5)
-
-def send_text(text):
+def send_text(text, ip_address, lecture_code):
+    url = f"http://{ip_address}:5000/transcription/{lecture_code}"
     data = {"text": text}
-    response = requests.post("http://api:5000/", json=data)
-    # print(f"Sent: {text}, Response: {response.json()}")
-
-def random_sentence_in_polish():
-    polish_sentences = [
-        "To jest losowe zdanie.",
-        "Zajebisty projekt!",
-        "Super sie na nim bawie",
-        "Losowe zdania jest super"
-    ]
-    return random.choice(polish_sentences)
-
-
-if __name__ == "__main__":
-    sender()
-
-# if __name__ == "__main__":
-#     process = multiprocessing.Process(target=sender)
-#     process.start()
-
-# FLOW: microphone(sender(transcribe->translate))
+    response = requests.post(url, json=data)
